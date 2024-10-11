6da881b287b9fe9a07d82f5a1d626c19217f2d16
Crewtlc <longhourswaltair@gmail.com>
2024-10-11 17:18:09 +0530

    Update BASCRIPT.js

--- BASCRIPT.js
+++ BASCRIPT.js
@@ -14,7 +14,7 @@ function validateForm() {
     alert("Please fill out all fields.");
     return false;
   }
- 
+}
 const scriptURL = 'https://script.google.com/macros/s/AKfycbwS_5bcH6JwUFUk6KeWfB20nQHzdpnks1ZOGrXVDQ8kDeFy0yMUFtZFVhd03dYIm04b/exec';
 const form = document.forms['baTestForm'];
 const submitButton = form.querySelector('button[type="submit"]');  // Select the submit button
