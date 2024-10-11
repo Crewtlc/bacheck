a3098084a2e7806e0e19c5e56586e7c3b8d3b42d
Crewtlc <longhourswaltair@gmail.com>
2024-10-11 18:46:46 +0530

    Update BASCRIPT.js

--- BASCRIPT.js
+++ BASCRIPT.js
@@ -4,13 +4,14 @@ function validateForm() {
   const lpName = document.getElementById("lpName").value;
   const lpCmsid = document.getElementById("lpCmsid").value;
   const alpName = document.getElementById("alpName").value;
+    const lpCmsid = document.getElementById("alpCmsid").value;
   const location = document.getElementById("location").value;
   const trainNo = document.getElementById("trainNo").value;
   const locoNo = document.getElementById("locoNo").value;
   const lpBaResult = document.getElementById("lpBaResult").value;
   const alpBaResult = document.getElementById("alpBaResult").value;
 
-  if (!cliName || !dateTime || !lpName || !lpCmsid || !alpName || !location || !trainNo || !locoNo || !lpBaResult || !alpBaResult) {
+  if (!cliName || !dateTime || !lpName || !lpCmsid || !alpName || !alpCmsid || !location || !trainNo || !locoNo || !lpBaResult || !alpBaResult) {
     alert("Please fill out all fields.");
     return false;
   }
